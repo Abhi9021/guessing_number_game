6bb77164dd006e04919f124bde5014f34c9825bd
Abhi9021 <104748269+Abhi9021@users.noreply.github.com>
2022-08-15 18:40:46 +0530

    Add files via upload

---
+++ Guessing_Number/src/main/java/com/mycompany/guessing_number/guessNumbers.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.guessing_number;
+
+/**
+ *
+ * @author hp
+ */
+class guessNumbers {
+    
+    private static int randomgen=0;
+    public  static int _randomNumber(){
+        randomgen=(int) Math.round(Math.random()*100);
+        return randomgen;
+    
+}
+    private static int checkifZero(int num){
+        int newNum=0;
+        if(num==0)
+        {
+         newNum=_randomNumber();
+        }
+        else
+        {
+            newNum=num;
+            
+        }
+        return newNum;
+    }
+}
+        
